6a445cab9a04e718b9fcf7ebf8ee09e805e7f2b7
Ali Kartal <ali_kartal91@hotmail.com>
2023-01-08 22:17:06 +0300

    list api issue fixed

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import axios from 'axios'
 
 function Section({ todos, setTodos, selectedTodos }) {
@@ -24,52 +24,74 @@ function Section({ todos, setTodos, selectedTodos }) {
     }
 
 
+    useEffect(() => {
+        setTimeout(() => {
+            for (let i = 0; i < 10; i++) {
+                axios.delete(`https://630f37fc37925634188a39d5.mockapi.io/todos/${i}`)
+                setTodos([
+                    {
+                        content: "Note1"
+                    },
+                    {
+                        content: "Note2"
+                    },
+                    {
+                        content: "Note3"
+                    },
+                    {
+                        content: "Note4"
+                    }
+                ])
+            }
+        }, 25000)
+    }, [setTodos])
+
     return (
         <div className='container'>
             <ul className='row mt-5'>
-                
-                { 
-                // LOADING SPINNER BOOTSTRAP
-                selectedTodos == "" ? 
 
-                <div className="spinner-border text-warning mt-5 ms-5" role="status">
-                    <span className="visually-hidden">Loading...</span>
-                </div>  : 
-                
-                    // TODOS MAP START
-                    selectedTodos.map((todo, i) => (
-                        <li key={i}
-                            className="col-md-8  me-md-auto     col-lg-6 mt-2"
-                        >
-                            <div className='d-flex '>
-                                
-                                <div className='form-check'>
-                                <input
-                                    className="toggle m-2 form-check-input"
-                                    type="checkbox" value="" id="flexCheckDefault"
-                                    checked={todo.isCompleted}
-                                    onChange={() => updateCompleteTodos(todo.content)}
-                                    />
+                {
+                    // LOADING SPINNER BOOTSTRAP
+                    selectedTodos == "" ?
+
+                        <div className="spinner-border text-warning mt-5 ms-5" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div> :
+
+                        // TODOS MAP START
+                        selectedTodos.map((todo, i) => (
+                            <li key={i}
+                                className="col-md-8  me-md-auto     col-lg-6 mt-2"
+                            >
+                                <div className='d-flex '>
+
+                                    <div className='form-check'>
+                                        <input
+                                            className="toggle m-2 form-check-input"
+                                            type="checkbox" value="" id="flexCheckDefault"
+                                            checked={todo.isCompleted}
+                                            onChange={() => updateCompleteTodos(todo.content)}
+                                        />
+                                    </div>
+
+                                    <label
+                                        className={todo.isCompleted === true ?
+                                            "text-decoration-line-through text-primary" : "text-dark"}
+                                    >
+                                        {todo.content}
+                                    </label>
+
+                                    <button
+                                        className={todo.isCompleted === true ?
+                                            "btn x-icon border border-0 text-danger" : "btn x-icon border border-0"}
+                                        onClick={() => deleteTodo(todo.id)}
+                                    ></button>
                                 </div>
-
-                                <label
-                                    className={todo.isCompleted === true ?
-                                        "text-decoration-line-through text-primary" : "text-dark"}
-                                >
-                                    {todo.content}
-                                </label>
-
-                                <button
-                                    className={todo.isCompleted === true ?
-                                        "btn x-icon border border-0 text-danger" : "btn x-icon border border-0"}
-                                    onClick={() => deleteTodo(todo.id)}
-                                ></button>
-                            </div>
-                        </li>
-                    ))
+                            </li>
+                        ))
                     // TODOS MAP FINISH
                 }
-                
+
             </ul>
         </div>
     )
